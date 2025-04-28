feb22b955ef3049535ddd90ae691824d40834f69
Ondra <okrejcik@gmail.com>
2025-04-28 12:50:11 +0200

    chore: add 1Password GPG key script and update recipe

---
+++ files/scripts/1password_gpg.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/keyrings/1password-archive-keyring.gpg
